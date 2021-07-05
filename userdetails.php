b229759546419c0d0a414ef70261395299a45a98
Arun manuel <66932711+ArunAlexMS@users.noreply.github.com>
2021-07-05 16:36:15 +0530

    Update userdetails.php

--- userdetails.php
+++ userdetails.php
@@ -251,7 +251,7 @@ button.btn-2:active:after {
       <!-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
         
       </button> -->
-      <div class="navbar-brand center" ><h5 style="margin-top:0px; ">Internship project by Sharwin Xavier R @ The Sparks Foundation</h5></div>
+      <div class="navbar-brand center" ><h5 style="margin-top:0px; ">Internship The Sparks Foundation</h5></div>
     </div> <!-- .navbar-header -->
    
   </div> <!-- .container -->
